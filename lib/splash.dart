b647107349c1897d35e5bc7abcad6bc1951f7952
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-11-06 21:36:27 +0545

    weather+

---
+++ lib/splash.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class Splash extends StatefulWidget {
+  const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    _nav();
+  }
+
+  _nav() async {
+    await Future.delayed(const Duration(milliseconds: 1500), () {});
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (context) => const MyHomePage(
+                  title: 'Hello',
+                )));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'Splash Screen',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
