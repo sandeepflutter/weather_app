f3d98b88b3193cae5ee13282e888f3728b2cebd3
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-11-07 23:17:59 +0545

    current location completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:weather_app/splash.dart';
 import 'package:weather_app/services.dart';
 import 'package:weather_app/weather.dart';
+import 'package:geolocator/geolocator.dart';
 
 void main() {
   runApp(const MyApp());
@@ -34,10 +35,59 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
 
+  var location="";
+
+  void getCurrentLocation() async{
+bool serviceEnabled;
+    LocationPermission permission;
+
+     // Test if location services are enabled.
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      // Location services are not enabled don't continue
+      // accessing the position and request users of the
+      // App to enable the location services.
+      return Future.error('Location services are disabled.');
+    }
+ 
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        // Permissions are denied, next time you could try
+        // requesting permissions again (this is also where
+        // Android's shouldShowRequestPermissionRationale
+        // returned true. According to Android guidelines
+        // your App should show an explanatory UI now.
+        return Future.error('Location permissions are denied');
+      }
+    }
+ 
+    if (permission == LocationPermission.deniedForever) {
+      // Permissions are denied forever, handle appropriately.
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request permissions.');
+    }
+
+    var position = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+
+setState(() {
+  location = "$position.latitude ,$position.longitude";
+});
+
+  }
+
   final _cityTextController = TextEditingController();
   final _dataService = DataService();
 
   WeatherResponse? _response;
+
+   @override
+  void initState() {
+    super.initState();
+   getCurrentLocation();
+
+  }
 
   @override
   void dispose(){
@@ -96,18 +146,20 @@ class _MyHomePageState extends State<MyHomePage> {
                 ),
               ),
 
+//Changing value of button as per Input text
               ValueListenableBuilder<TextEditingValue>(
         valueListenable: _cityTextController,
         builder: (context, value, child) {
           return ElevatedButton(
             onPressed: _search,
-            
             child: value.text.isNotEmpty ? const Text('UPDATE'):const Text('SAVE'),
           );
         },
       ),
-              // ElevatedButton(
-              //   onPressed: _search, child: const Text('Search'))
+
+        Text("Position: $location"),
+      
+      
             ],
           ),
         ),
