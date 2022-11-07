dd84eb510bfeaa5d2ab576cf742f837bf3535722
sandeepflutter <sandeepbudha.sb@gmail.com>
2022-11-07 10:28:09 +0545

    splash screen

--- lib/splash.dart
+++ lib/splash.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'main.dart';
 
+//changing state we need stateful widget
 class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
 
@@ -10,15 +11,21 @@ class Splash extends StatefulWidget {
 }
 
 class _SplashState extends State<Splash> {
+
+  //function to run first time after class is called
   @override
   void initState() {
     super.initState();
     _nav();
   }
 
+//we are async because we are using time
   _nav() async {
     await Future.delayed(const Duration(milliseconds: 5000), () {});
-    if (!mounted) return;
+    //If a StatefulWidget uses BuildContext, the mounted property has to be checked after an asynchronous gap.
+    if (!mounted) return; 
+  
+//pushreplacement to permantly reach directed screen
     Navigator.pushReplacement(
         context,
         MaterialPageRoute(
@@ -29,13 +36,50 @@ class _SplashState extends State<Splash> {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text(
-          'Splash Screen',
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
+    return  Scaffold(
+      body: Container(
+        //background filled image
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: 
+          AssetImage(
+              'assets/bg.png',
+            ),
+            fit: BoxFit.fill,
+            )
+            
+        ),
+        child: Center(
+
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.all(40.0),
+                //Display text
+                child:  Text(
+                  'WE SHOW WEATHER FOR YOU',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.red,
+                  ),
+                ),
+              ),
+
+          //Skip button
+            const SizedBox(height: 120),
+            OutlinedButton.icon(
+              icon: const Icon(Icons.skip_next, size: 18),
+              label: const Text('Skip'),
+              onPressed: () {
+                Navigator.pushReplacement(
+                context,
+                MaterialPageRoute(builder: (context) => const MyHomePage(title: 'Hello',)),
+              );
+              },
+            ),
+            ],
           ),
         ),
       ),
